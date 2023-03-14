a9838279ad83913c71ab0afa62184e45f973326e
Adrian Roszkowski <adr11@hi.is>
2023-03-14 14:10:37 +0000

    Added MathObject

---
+++ src/main/java/is/hi/hbv202g/ass9/compositeLeafObserverTemplateMethod/MathObject.java
@@ -0,0 +1,16 @@
+package is.hi.hbv202g.ass9.compositeLeafObserverTemplateMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class MathObject implements MathExpression, Observer {
+    protected List<NumberLeaf> values;
+
+    public MathObject(){
+        values = new ArrayList<NumberLeaf>();
+    }
+
+    public void add(NumberLeaf value){
+        values.add(value);
+    }
+}
--- src/main/java/is/hi/hbv202g/ass9/compositeLeafObserverTemplateMethod/MultiplyAndPlus.java
+++
@@ -1,7 +0,0 @@
-package is.hi.hbv202g.ass9.compositeLeafObserverTemplateMethod;
-
-public abstract class  MultiplyAndPlus{
-    protected MultiplyComposite multiplyComposite;
-    protected PlusComposite plusComposite;
-
-}
